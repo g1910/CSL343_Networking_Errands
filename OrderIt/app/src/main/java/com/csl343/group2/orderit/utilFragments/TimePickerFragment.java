9df6844ecfbf32b0443d9ef036d2708167c4050b
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-15 19:50:08 +0530

    Add TimePickerFragment

---
+++ OrderIt/app/src/main/java/com/csl343/group2/orderit/utilFragments/TimePickerFragment.java
@@ -0,0 +1,66 @@
+package com.csl343.group2.orderit.utilFragments;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.Message;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import com.csl343.group2.orderit.R;
+
+import java.security.MessageDigest;
+import java.util.Calendar;
+import java.util.logging.Handler;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    OnTimeSetListener listener;
+
+    public TimePickerFragment() {
+        // Required empty public constructor
+    }
+
+    public interface OnTimeSetListener {
+        public void onTimePicked(int hourOfDay, int minute);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            listener = (OnTimeSetListener) activity;
+        }catch (ClassCastException e){
+            throw new ClassCastException(activity.toString() + " must implement OnTimeSetListener");
+        }
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        listener.onTimePicked(hourOfDay,minute);
+    }
+}
