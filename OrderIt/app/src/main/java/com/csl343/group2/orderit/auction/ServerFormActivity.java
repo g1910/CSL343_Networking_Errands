12220cc31aab3e16bd3306b07129ad8bd9881bc9
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-16 16:11:36 +0530

    Add functionality to start auction

--- OrderIt/app/src/main/java/com/csl343/group2/orderit/auction/ServerFormActivity.java
+++ OrderIt/app/src/main/java/com/csl343/group2/orderit/auction/ServerFormActivity.java
@@ -11,43 +11,63 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.csl343.group2.orderit.R;
 import com.csl343.group2.orderit.utilFragments.DatePickerFragment;
+import com.csl343.group2.orderit.utilFragments.ServerConnect;
 import com.csl343.group2.orderit.utilFragments.TimePickerFragment;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class ServerFormActivity extends FragmentActivity implements TimePickerFragment.OnTimeSetListener, DatePickerFragment.OnDateSetListener{
 
     TextView aucEndTime,aucEndDate,aucExpTime,aucExpDate;
+    EditText aucDesc, aucLocation;
+    Button aucStartBtn;
     Calendar c = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_form);
-        String time = String.format("%2d:%2d",c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE));
+
+        aucDesc = (EditText) findViewById(R.id.auction_desc);
+        aucLocation = (EditText) findViewById(R.id.auction_location);
+
+        String time = String.format("%02d:%02d",c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE));
+        String date = String.format("%02d-%02d-%04d",c.get(Calendar.DAY_OF_MONTH),c.get(Calendar.MONTH),c.get(Calendar.YEAR));
         aucEndTime = (TextView) findViewById(R.id.auc_end_time);
         aucEndTime.setText(time);
         aucEndTime.setOnClickListener(showTimePickerDialog);
 
         aucEndDate = (TextView) findViewById(R.id.auc_end_date);
-        aucEndDate.setText(c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.MONTH) + "-"  + c.get(Calendar.YEAR)) ;
+        aucEndDate.setText(date) ;
         aucEndDate.setOnClickListener(showDatePickerDialog);
 
         aucExpTime = (TextView) findViewById(R.id.auc_exp_time);
-        aucExpTime.setText(c.get(Calendar.HOUR_OF_DAY) + ":"  + c.get(Calendar.MINUTE));
+        aucExpTime.setText(time);
         aucExpTime.setOnClickListener(showTimePickerDialog);
 
         aucExpDate = (TextView) findViewById(R.id.auc_exp_date);
-        aucExpDate.setText(c.get(Calendar.DAY_OF_MONTH) + "-" + c.get(Calendar.MONTH) + "-"  + c.get(Calendar.YEAR)) ;
+        aucExpDate.setText(date) ;
         aucExpDate.setOnClickListener(showDatePickerDialog);
+
+        aucStartBtn = (Button) findViewById(R.id.auc_start_btn);
+        aucStartBtn.setOnClickListener(startNewAuction);
+
 
     }
 
@@ -123,9 +143,23 @@ public class ServerFormActivity extends FragmentActivity implements TimePickerFr
         }
     };
 
+    View.OnClickListener startNewAuction = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("location", aucLocation.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("endtime",aucEndDate.getText().toString() + " " + aucEndTime.getText().toString()+":00"));
+            nameValuePairs.add(new BasicNameValuePair("expected",aucExpDate.getText().toString() + " " + aucExpTime.getText().toString()+":00"));
+            nameValuePairs.add(new BasicNameValuePair("description",aucDesc.getText().toString()));
+            nameValuePairs.add(new BasicNameValuePair("id_user","1"));
+            ServerConnect myServer=new ServerConnect();
+            myServer.execute("http://10.20.9.85/Networks/CSL343_Networking_Errands/Server/auction.php",nameValuePairs,this);
+        }
+    };
+
     @Override
     public void onTimePicked(int view,int hourOfDay, int minute) {
-        String time = String.format("%2d:%2d",hourOfDay,minute);
+        String time = String.format("%02d:%02d",hourOfDay,minute);
         switch(view){
             case R.id.auc_end_time:aucEndTime.setText(time);break;
             case R.id.auc_exp_time:aucExpTime.setText(time);break;
@@ -135,7 +169,7 @@ public class ServerFormActivity extends FragmentActivity implements TimePickerFr
 
     @Override
     public void onDatePicked(int view,int year, int monthOfYear, int dayOfMonth) {
-        String date = String.format("%2d-%2d-%4d",dayOfMonth,monthOfYear,year);
+        String date = String.format("%02d-%02d-%04d",dayOfMonth,monthOfYear,year);
         switch(view){
             case R.id.auc_end_date:aucEndDate.setText(date);break;
             case R.id.auc_exp_date:aucExpDate.setText(date);break;
