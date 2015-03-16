77bfe497379fadc6c403c1be580613d8d7096d5d
ByteLandian <mohitg@iitrpr.ac.in>
2015-03-16 16:54:37 +0530

    Returned Success message

--- Server/auction.php
+++ Server/auction.php
@@ -1,6 +1,5 @@
 <?php
 include 'config.php';
-$id=$_POST['id_auction'];
 $location=$_POST['location'];
 $endtime=$_POST['endtime'];
 $expectedtime=$_POST['expected'];
@@ -11,6 +10,9 @@ $con=mysqli_connect($IP,$user,$pass,$db);
 
 $starttime= date( 'Y-m-d H:i:s', time());
 
-mysqli_query($con,"INSERT INTO `Auction`(`idAuction`, `location`, `start_time`, `end_time`, `expctd_time`, `description`, `User_idUser`) VALUES (\"$id\",\"$location\",\"$starttime\",\"$endtime\",\"$expectedtime\",\"$description\",\"$userid\")")  or die(mysqli_error($con));
-
+mysqli_query($con,"INSERT INTO `Auction`( `location`, `start_time`, `end_time`, `expctd_time`, `description`, `User_idUser`) VALUES (\"$location\",\"$starttime\",\"$endtime\",\"$expectedtime\",\"$description\",\"$userid\")")  or die(mysqli_error($con));
+$arr = array(
+    "message" => "Success", 
+);
+print(json_encode($output));
 ?>
