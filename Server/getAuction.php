a79fa3be6416145031ae1312ff9bb7aa47c404ac
ByteLandian <mohitg@iitrpr.ac.in>
2015-03-16 20:29:03 +0530

    Current auction not running added

--- Server/getAuction.php
+++ Server/getAuction.php
@@ -4,12 +4,30 @@
 	$time=date( 'Y-m-d H:i:s', time());
 	$con=mysqli_connect($IP,$user,$pass,$db);
 	$result=mysqli_query($con,"select `idAuction`, `location`, `start_time`, `expctd_time`, `description` from `Auction` where `User_idUser`=\"$user_id\" and `end_time`>=\"$time\"") or die("Error: ".mysqli_error($con));
-	$arr = array(
-	    "isRunning" => "False", 
-	);
-	$output[]=$arr;
-	while ($row=mysqli_fetch_assoc($result))
-		$output[]=$row;
+	
+	$num=mysqli_num_rows($result);
+
+	if ($num==0)
+	{
+		$arr = array(
+		    "isRunning" => "False", 
+		);
+		$output[]=$arr;
+	}
+	else
+	{
+		$arr = array(
+		    "isRunning" => "True", 
+		);
+		$output[]=$arr;
+		while ($row=mysqli_fetch_assoc($result))
+		{
+			$output[]=$row;
+			$auctionId=$row['idAuction'];
+		}
+//		echo $auctionId;
+	}
+
 	print(json_encode($output));
 	mysqli_close($con);
 ?>
