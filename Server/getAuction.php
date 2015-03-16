d60ffa2ef462787004310724a43e60758ed0d2fb
ByteLandian <mohitg@iitrpr.ac.in>
2015-03-16 17:37:14 +0530

    Get Auction File Updated

--- Server/getAuction.php
+++ Server/getAuction.php
@@ -3,7 +3,7 @@
 	$user_id=$_GET['user_id'];
 	$time=date( 'Y-m-d H:i:s', time());
 	$con=mysqli_connect($IP,$user,$pass,$db);
-	$result=mysqli_query($con,"select `idAuction`, `location`, `start_time`, `expctd_time`, `description` from `Auction` where `User_idUser`=\"$user_id\" and `end_time`<=\"$time\"") or die("Error: ".mysqli_error($con));
+	$result=mysqli_query($con,"select `idAuction`, `location`, `start_time`, `expctd_time`, `description` from `Auction` where `User_idUser`=\"$user_id\" and `end_time`>=\"$time\"") or die("Error: ".mysqli_error($con));
 	while ($row=mysqli_fetch_assoc($result))
 		$output[]=$row;
 	print(json_encode($output));
