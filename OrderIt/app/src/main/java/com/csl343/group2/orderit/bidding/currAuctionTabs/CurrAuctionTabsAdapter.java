597dd31dd136544466afd200bcb14ef5a73ee54f
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-16 20:30:56 +0530

    Add classes for Currently Running Auctions page with tabs

---
+++ OrderIt/app/src/main/java/com/csl343/group2/orderit/bidding/currAuctionTabs/CurrAuctionTabsAdapter.java
@@ -0,0 +1,37 @@
+package com.csl343.group2.orderit.bidding.currAuctionTabs;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by gaurav on 16/3/15.
+ */
+public class CurrAuctionTabsAdapter extends FragmentPagerAdapter{
+    public CurrAuctionTabsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0: return new ToParticipateFragment();
+            case 1: return new CurrParticipatingFragment();
+        };
+        return null;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position){
+            case 0: return "To Participate";
+            case 1: return "Participating";
+        };
+        return super.getPageTitle(position);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
