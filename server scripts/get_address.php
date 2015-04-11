e658d5b7305898ca4a121e8bdb530ff27986a014
kunal1995 <kunaly1995@gmail.com>
2015-04-11 19:23:37 +0530

    get_phone.php and get_address.php server scripts added

---
+++ server scripts/get_address.php
@@ -0,0 +1,35 @@
+<?php
+ 
+
+
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+
+$response = array();
+
+
+ $email = $_POST['email'];
+     
+
+ $res = mysql_query("SELECT address FROM User WHERE emailId='$email'")or die(mysql_error()) ;
+ 
+ if (mysql_num_rows($res) > 0) {
+	$result=mysql_fetch_array($res);
+	$result=$result['address'];
+	
+    echo $result;
+}
+
+else
+{
+ $result="null";
+  echo $result;
+
+}
+
+
+?>
