0b3a8d74548382fd7f9af48aecdaaf3cf7e4187f
ByteLandian <mohitg@iitrpr.ac.in>
2015-04-14 01:36:00 +0530

    Get all running auctions

---
+++ Server/getAllAuctions.php
@@ -0,0 +1,35 @@
+<?php
+	include 'config.php';
+	date_default_timezone_set("Asia/Kolkata");
+	$time=date( 'Y-m-d H:i:s', time());
+//	echo $time;
+	$con=mysqli_connect($IP,$user,$pass,$db);
+//	echo "select `idAuction`, `location`, `start_time`, `expctd_time`, `description` from `Auction` where `idUser`=\"$user_id\" and `end_time`>=\"$time\"";
+	$result=mysqli_query($con,"select `idAuction`, `idUser`, `location`, `start_time`, `expctd_time`, `description` from `Auction` where `end_time`>=\"$time\"") or die("Error: ".mysqli_error($con));
+	
+	$num=mysqli_num_rows($result);
+
+	if ($num==0)
+	{
+		$arr = array(
+		    "isRunning" => "False", 
+		);
+		$output[]=$arr;
+	}
+	else
+	{
+		$arr = array(
+		    "isRunning" => "True", 
+		);
+		$output[]=$arr;
+		while ($row=mysqli_fetch_assoc($result))
+		{
+			$output[]=$row;
+			$auctionId=$row['idAuction'];
+		}
+//		echo $auctionId;
+	}
+
+	print(json_encode($output));
+	mysqli_close($con);
+?>
