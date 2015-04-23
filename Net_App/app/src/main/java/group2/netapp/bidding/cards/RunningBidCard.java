98c3d10aae4bde2bfede588e14bff64aa1edff1a
ByteLandian <mohitg@iitrpr.ac.in>
2015-04-23 17:21:18 +0530

    Added Running Bids Files

---
+++ Net_App/app/src/main/java/group2/netapp/bidding/cards/RunningBidCard.java
@@ -0,0 +1,60 @@
+package group2.netapp.auction.cards;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import group2.netapp.R;
+import it.gmariotti.cardslib.library.internal.Card;
+
+/**
+ * Created by mohit on 12/4/15.
+ */
+public class RunningBidCard extends Card {
+
+    String bidLocation, bidOrder;
+    int bidId;
+    public RunningBidCard(Context context, int bidId, String bidLocation, String bidOrder) {
+        super(context, R.layout.card_auction_bid);
+
+        this.bidLocation = bidLocation;
+        this.bidOrder = bidOrder;
+        this.bidId = bidId;
+    }
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        super.setupInnerViewElements(parent, view);
+
+        TextView bidLocationView = (TextView) parent.findViewById(R.id.auc_bid_location);
+        TextView bidOrderView = (TextView) parent.findViewById(R.id.auc_bid_order_summary);
+
+        bidLocationView.setText(bidLocation);
+        bidOrderView.setText(bidOrder);
+    }
+
+    public String getBidLocation() {
+        return bidLocation;
+    }
+
+    public void setBidLocation(String bidLocation) {
+        this.bidLocation = bidLocation;
+    }
+
+    public String getBidOrder() {
+        return bidOrder;
+    }
+
+    public void setBidOrder(String bidOrder) {
+        this.bidOrder = bidOrder;
+    }
+
+    public int getBidId() {
+        return bidId;
+    }
+
+    public void setBidId(int bidId) {
+        this.bidId = bidId;
+    }
+}
