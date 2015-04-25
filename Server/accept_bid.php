6e6ee6b34eb3fe33ad3afff8c7d183e5fe67ddb3
Gaurav <gaurav.mittal.191013@gmail.com>
2015-04-26 03:31:46 +0530

    Modified code to add auction category in the Placed table for the accepted bid

--- Server/accept_bid.php
+++ Server/accept_bid.php
@@ -5,11 +5,11 @@
 
 	$id_auc = $_GET['id_auc'];
 	$id_bid = $_GET['id_bid'];
+	$id_cat = $_GET['id_cat'];
 
-	$result=mysqli_query($con,"update `Placed` set status='A' where idBid=".$id_bid." and idAuction=".$id_auc) or die("Error: ".mysqli_error($con));
+	$result=mysqli_query($con,"update `Placed` set status='A', idCategory=".$id_cat." where idBid=".$id_bid." and idAuction=".$id_auc) or die("Error: ".mysqli_error($con));
 
 	$bid_request = array('tag' => 'bid_request', );
-
 	$output[] = $bid_request;
 
 	if($result){
