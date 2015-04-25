330b61d9a0973f8b6c407379f80b9d16e7f65a84
Gaurav <gaurav.mittal.191013@gmail.com>
2015-04-26 00:23:56 +0530

    Added code to insert an initial auction category with auction creation

--- Server/auction.php
+++ Server/auction.php
@@ -5,15 +5,27 @@ $endtime=$_GET['endtime'];
 $expectedtime=$_GET['expected'];
 $description=$_GET['description'];
 $userid=$_GET['id_user'];
+$min_price = $_GET['min_price'];
 $con=mysqli_connect($IP,$user,$pass,$db);
 
 date_default_timezone_set("Asia/Kolkata");
 $starttime= date( 'Y-m-d H:i:s', time());
 
+mysqli_autocommit($con,FALSE);
+
 mysqli_query($con,"INSERT INTO `Auction`( `location`, `start_time`, `end_time`, `expctd_time`, `description`, `idUser`) VALUES (\"$location\",\"$starttime\",\"$endtime\",\"$expectedtime\",\"$description\",\"$userid\")")  or die(mysqli_error($con));
-$arr = array(
+
+mysqli_query($con,"INSERT INTO `Auction_Categories`(`idAuction`,`minPrice`) VALUES (".mysqli_insert_id($con).",".$_GET['min_price'].")") or die(mysqli_error($con));
+
+if(!mysqli_commit($con)){
+	$arr = array(
     "message" => "Success", 
-);
+	);
+}else{
+	$arr = array(
+	    "message" => "Success", 
+	);
+}
 $output[]=$arr;
 print(json_encode($output));
 ?>
