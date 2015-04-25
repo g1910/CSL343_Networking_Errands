6d89cf17a815aa235ba01a3253db790f1a4c9da8
Gaurav <gaurav.mittal.191013@gmail.com>
2015-04-26 02:11:44 +0530

    Added functionality to accept bid in a particular category

--- Net_App/app/src/main/java/group2/netapp/auction/AucBidsFragment.java
+++ Net_App/app/src/main/java/group2/netapp/auction/AucBidsFragment.java
@@ -1,6 +1,8 @@
 package group2.netapp.auction;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,6 +12,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +40,7 @@ public class AucBidsFragment extends Fragment {
     JSONObject bid;
     boolean isRequest;
     JSONArray aucCategories;
+    RadioGroup rG;
     public AucBidsFragment() {
         // Required empty public constructor
     }
@@ -62,6 +68,7 @@ public class AucBidsFragment extends Fragment {
         aucCategories = ((AuctionActivity)getActivity()).getRunningBids();
         location = (TextView) v.findViewById(R.id.auc_bidview_loc);
         order = (TextView) v.findViewById(R.id.auc_bidview_order);
+
 
         Bundle b = getArguments();
         isRequest = b.getBoolean("isRequest",false);
@@ -94,22 +101,6 @@ public class AucBidsFragment extends Fragment {
         return super.onOptionsItemSelected(item);
     }
 
-    public void acceptBid(){
-
-        try {
-            int auctionId = ((AuctionActivity)getActivity()).getAuctionDetails().getInt("idAuction");
-            int bidId = bid.getInt("idBid");
-            ServerConnect myServer=new ServerConnect(getActivity());
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-            nameValuePairs.add(new BasicNameValuePair("id_auc",auctionId+""));
-            nameValuePairs.add(new BasicNameValuePair("id_bid",bidId+""));
-            Log.d("AuctionActivity",getString(R.string.IP)+"accept_bid.php");
-            myServer.execute(getString(R.string.IP)+"accept_bid.php",nameValuePairs);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
     public void rejectBid(){
         try {
             int auctionId = ((AuctionActivity)getActivity()).getAuctionDetails().getInt("idAuction");
@@ -124,5 +115,66 @@ public class AucBidsFragment extends Fragment {
             e.printStackTrace();
         }
     }
+
+    public void acceptBid(){
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.dialog_auc_category,null);
+        rG = (RadioGroup) v.findViewById(R.id.auc_category_rg);
+        populateRadioButtons();
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Select Auction Category")
+                .setView(v)
+                .setPositiveButton("Accept",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        try {
+                            int auctionId = ((AuctionActivity)getActivity()).getAuctionDetails().getInt("idAuction");
+                            int bidId = bid.getInt("idBid");
+                            int catId = aucCategories.getJSONObject(rG.getCheckedRadioButtonId()%100).getInt("idCategory");
+                            ServerConnect myServer=new ServerConnect(getActivity());
+                            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                            nameValuePairs.add(new BasicNameValuePair("id_auc",auctionId+""));
+                            nameValuePairs.add(new BasicNameValuePair("id_bid",bidId+""));
+                            nameValuePairs.add(new BasicNameValuePair("id_cat",catId+""));
+                            Log.d("AuctionActivity",getString(R.string.IP)+"accept_bid.php");
+                            myServer.execute(getString(R.string.IP)+"accept_bid.php",nameValuePairs);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).setNeutralButton("New", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+     }
+
+    public void populateRadioButtons(){
+        for(int i = 0;i<aucCategories.length();++i){
+            RadioButton btn = new RadioButton(getActivity());
+            btn.setId(100+i);
+            try {
+                btn.setText("Base Bid: \u20B9 "+aucCategories.getJSONObject(i).getInt("minPrice"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            rG.addView(btn);
+
+        }
+        ((RadioButton)rG.getChildAt(0)).setChecked(true);
+    }
+
+
 }
 
