6d563c7330335d567ce9c5209194500d3a483be0
ByteLandian <mohitg@iitrpr.ac.in>
2015-04-27 19:51:32 +0530

    Added place Bid Menu

--- Net_App/app/src/main/java/group2/netapp/bidding/PlaceRunningBid.java
+++ Net_App/app/src/main/java/group2/netapp/bidding/PlaceRunningBid.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,8 +28,25 @@ public class PlaceRunningBid extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place_running_bid, container, false);
+        setHasOptionsMenu(true);
+        View v= inflater.inflate(R.layout.fragment_place_running_bid, container, false);
+        return v;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.place_bid,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.place_bid:
+               // newBid();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
