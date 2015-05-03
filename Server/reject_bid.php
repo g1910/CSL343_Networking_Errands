582d8f90c9eb30ff7ddb194bfe5503397ed670a3
Gaurav <gaurav.mittal.191013@gmail.com>
2015-05-03 15:21:28 +0530

    Modified to delete the bid entry instead of updating it

--- Server/reject_bid.php
+++ Server/reject_bid.php
@@ -6,7 +6,7 @@
 	$id_auc = $_GET['id_auc'];
 	$id_bid = $_GET['id_bid'];
 
-	$result=mysqli_query($con,"update `Placed` set status='R' where idBid=".$id_bid." and idAuction=".$id_auc) or die("Error: ".mysqli_error($con));
+	$result=mysqli_query($con,"delete from `Placed` where idBid=".$id_bid." and idAuction=".$id_auc) or die("Error: ".mysqli_error($con));
 
 	$bid_request = array('tag' => 'bid_reject', );
 	$output[] = $bid_request;
