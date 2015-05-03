ed5a3fb974aafeb75378993eaaccb922d5826fbb
Gaurav <gaurav.mittal.191013@gmail.com>
2015-05-03 15:22:08 +0530

    Added tag and extra attribute in the insert query

--- Server/auction.php
+++ Server/auction.php
@@ -6,6 +6,7 @@
 	$description=$_GET['description'];
 	$userid=$_GET['id_user'];
 	$min_price = $_GET['min_price'];
+	$order_limit = $_GET['order_limit'];
 	$con=mysqli_connect($IP,$user,$pass,$db);
 
 	date_default_timezone_set("Asia/Kolkata");
@@ -13,9 +14,14 @@
 
 	mysqli_autocommit($con,FALSE);
 
-	mysqli_query($con,"INSERT INTO `Auction`( `location`, `start_time`, `end_time`, `expctd_time`, `description`, `idUser`) VALUES (\"$location\",\"$starttime\",\"$endtime\",\"$expectedtime\",\"$description\",\"$userid\")")  or die(mysqli_error($con));
+	mysqli_query($con,"INSERT INTO `Auction`( `location`, `start_time`, `end_time`, `expctd_time`, `description`, `idUser`,`minPrice`,`orderLimit`) VALUES (\"$location\",\"$starttime\",\"$endtime\",\"$expectedtime\",\"$description\",\"$userid\",\"$min_price\",\"$order_limit\")")  or die(mysqli_error($con));
 
 	mysqli_query($con,"INSERT INTO `Auction_Categories`(`idAuction`,`minPrice`) VALUES (".mysqli_insert_id($con).",".$_GET['min_price'].")") or die(mysqli_error($con));
+
+	$load = array(
+		    "tag" => "createAuction", 
+		);
+	$output[]=$load;
 
 	if(!mysqli_commit($con)){
 		$status = array('status' => false, );
@@ -24,5 +30,6 @@
 	}
 	$output[]=$status;
 
-	echo json_encode($output));
+	echo json_encode($output);
+	mysqli_close($con);
 ?>
