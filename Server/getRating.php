533287b61784fcbb554902b81fd7da2f46faec44
Mohit Garg <Bytelandian@users.noreply.github.com>
2015-05-03 14:43:14 +0530

    update getRating.php role and status

--- Server/getRating.php
+++ Server/getRating.php
@@ -5,7 +5,7 @@
 		include 'config.php';
 		$con=mysqli_connect($IP,$user,$pass,$db);
 //		echo "Select avg(stars) as rating,count(stars) as numRated from `Feedback` group by `idReceiver` having `idReceiver`=$a";
-		$result=mysqli_query($con,"Select avg(stars) as rating,count(stars) as numRated from `Feedback` group by `idReceiver` having `idReceiver`=$a") or die(mysqli_error($con));
+		$result=mysqli_query($con,"Select avg(stars) as rating,count(stars) as numRated from `Feedback` WHERE status=1 and role=0 group by `idReceiver` having `idReceiver`=$a") or die(mysqli_error($con));
 
 		$output=[];
 		while ($row=mysqli_fetch_assoc($result))
